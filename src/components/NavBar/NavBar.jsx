f7d93a9fcf722447be5a4127f3c3006de4d4b53c
Jose Luis Neira <jlneira99@gmail.com>
2024-08-20 10:16:02 +0200

    include navbar random page to take you to random MoodPage

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,23 @@
-import { Link } from "react-router-dom";
-import styles from "./NavBar.module.scss"
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from './NavBar.module.scss';
 
-export default function NavBar () {
+export default function NavBar() {
+    const navigate = useNavigate();
+
+    const handleRandomClick = () => {
+        const moodId = Math.floor(Math.random() * 5) + 1; 
+        navigate(`/${moodId}`);
+    };
+
     return (
         <div className={styles.NavBar}>
             <div><Link to="/"><h1>Moodify</h1></Link></div>
             <div className={styles.navOptions}>
                 <Link to="*"><h2>Playlist</h2></Link>
-                <Link to="*"><h2>Random</h2></Link>
+                <button onClick={handleRandomClick}><h2>Random</h2></button>
                 <Link to="/about"><h2>About</h2></Link>
             </div>
         </div>
-    )
+    );
 }
--- src/pages/MoodPage/MoodPage.jsx
+++ src/pages/MoodPage/MoodPage.jsx
@@ -1,27 +1,38 @@
-
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Sidebar from "../../components/Sidebar/Sidebar";
 import styles from "./MoodPage.module.scss";
+import { useEffect, useState } from 'react';
 
-export default function MoodPage() {
-  const location = useLocation();
+export default function MoodPage({ moodList }) {
+  const { moodId } = useParams();
   const navigate = useNavigate();
+  const [moodDetails, setMoodDetails] = useState(null);
 
-  // If location.state is null, navigate back to HomePage
-  if (!location.state || !location.state.details) {
-    // Optionally, you can show an error message instead of navigating
-    navigate('/');
-    return null;
+  useEffect(() => {
+    if (moodList) {
+      const mood = moodList.find((m) => m.id === parseInt(moodId));
+      if (mood) {
+        setMoodDetails(mood);
+      } else {
+        navigate('/'); 
+      }
+    }
+  }, [moodId, moodList, navigate]);
+
+  if (moodList === null) {
+    return <div>Loading...</div>; 
   }
 
-  const { details } = location.state;
+  if (!moodDetails) {
+    return <div>Loading...</div>; 
+  }
 
   return (
     <div className={styles.MoodPage}>
       <Sidebar />
       <div className={styles.main}>
         <div className={styles.header}>
-          <h1>{details.title}</h1>
+          <h1>{moodDetails.title}</h1>
         </div>
       </div>
     </div>
