e5273ebf8ca7bb56963347c760c0396afd5d6733
Mael Micout <gh api micoutmael3@gmail.com>
2024-08-20 19:56:30 +0200

    styling about page done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import styles from "./Footer.module.scss"
 
-export default function Footer () {
-    return (
-        <div className={styles.Footer}>
-           <a href="https://github.com/moodify-org/moodify" target="blank">GitHub Repository</a> 
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div className={styles.Footer}>
+      <a href="https://github.com/moodify-org/moodify" target='_blank'>
+        <i className="fa-brands fa-github"></i><span></span></a>
+    </div>
+  )
 }
--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -1,23 +1,45 @@
 import styles from "./AboutPage.module.scss"
 
 export default function AboutPage() {
-    return (
-        <div className={styles.AboutPage}>
-            <div className={styles.aboutInfo}>
-                <h1>What is Moodify?</h1>
-                <h3>Moodify is an easy-to-use application where you can select the mood you are feeling and songs will be automatically recommended to you. You are also able to create personalised playlists with these available songs to specify a particular vibe you are looking for!</h3>
+  return (
+    <div className={styles.AboutPage}>
+      <div className={styles.aboutInfo}>
+        <h1>What is Moodify?</h1>
+        <h3>Moodify is an easy-to-use application where you can select the mood you are feeling and songs will be automatically recommended to you. You are also able to create personalised playlists with these available songs to specify a particular vibe you are looking for!</h3>
+      </div>
+      <div className={styles.aboutCreators}>
+        <h1>Who made Moodify?</h1>
+        <div className={styles.aboutCreatorsDiv}>
+          <a
+            href="https://github.com/jlneira-dev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.aboutCard}
+          >
+            <div className={styles.cardImage}>
+              <img src="jose.JPG" alt="Jose" />
             </div>
-            <div className={styles.aboutCreators}>
-                <h1>Who made Moodify?</h1>
-                <div className={styles.aboutCreatorsDiv}>
-                    <div className={styles.aboutJose}> 
-                        <h3>Jose</h3>
-                    </div>
-                    <div className={styles.aboutMael}> 
-                        <h3>Mael</h3>
-                    </div>
-                </div>
+            <div className={styles.cardContent}>
+              <h3>Jose</h3>
+              <p>Developer and designer of Moodify.</p>
             </div>
+          </a>
+          <a
+            href="https://github.com/Mael1701"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.aboutCard}
+          >
+            <div className={styles.cardImage}>
+              <img src="mael.jpg" alt="Mael" />
+            </div>
+            <div className={styles.cardContentMael}>
+              <h3>Mael</h3>
+              <p>Developer and designer of Moodify.</p>
+            </div>
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
