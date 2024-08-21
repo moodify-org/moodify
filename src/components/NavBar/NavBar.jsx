d5dabed9f6f534ac14faae8a77a24b424ee57c8e
Mael Micout <gh api micoutmael3@gmail.com>
2024-08-21 19:27:35 +0200

    navbar links done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styles from './NavBar.module.scss';
 
 export default function NavBar() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleRandomClick = () => {
-        const moodId = Math.floor(Math.random() * 5) + 1; 
-        navigate(`/${moodId}`);
-    };
+  const getRandomMoodId = () => {
+    return Math.floor(Math.random() * 5) + 1;
+  };
 
-    return (
-        <div className={styles.NavBar}>
-            <div><Link to="/"><h1>Moodify</h1></Link></div>
-            <div className={styles.navOptions}>
-                <Link to="/create"><h2>Create Playlist</h2></Link>
-                <button onClick={handleRandomClick}><h2>Random</h2></button>
-                <Link to="/about"><h2>About</h2></Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.NavBar}>
+      <div><Link to="/"><h1>Moodify</h1></Link></div>
+      <div className={styles.navOptions}>
+        <Link to="/create"><h2>Create Playlist</h2></Link>
+        <Link to={`/${getRandomMoodId()}`}><h2>Random</h2></Link>
+        <Link to="/about"><h2>About</h2></Link>
+      </div>
+    </div>
+  );
 }
