1140008caf4dca16c77454ea8ad183d8afb6782f
Mael Micout <gh api micoutmael3@gmail.com>
2024-08-21 16:00:52 +0200

    solve merge problems

--- src/pages/AddPlaylist/AddPlaylistPage.jsx
+++ src/pages/AddPlaylist/AddPlaylistPage.jsx
@@ -12,7 +12,6 @@ export default function AddPlaylistPage({ playlistList, callbackToCreate }) {
   const handleSubmit = e => {
     e.preventDefault();
 
-<<<<<<< HEAD
     const songDetails = {
       title: title,
       description: description,
@@ -29,85 +28,36 @@ export default function AddPlaylistPage({ playlistList, callbackToCreate }) {
   }
 
   return (
-    <section className="AddMovie box">
-      <h2>Create your own playlist:</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Title:
-          <input
-            type="text"
-            name="title"
-            required
-            placeholder="Favourites"
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-          />
-        </label>
-        <label>
-          Description:
-          <input
-            type="text"
-            name="description"
-            placeholder="Songs that I love"
-            value={description}
-            onChange={e => setDescription(e.target.value)}
-          />
-        </label>
-        <p>
-          <button>Create</button>
-        </p>
-      </form>
+    <section className={styles.AddPlaylistPage}>
+      <div className={styles.div}>
+        <h2>Create your own playlist: </h2>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <label>
+            Title: <br></br>
+            <input
+              type="text"
+              name="title"
+              required
+              placeholder="Favourites"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            />
+          </label>
+          <label>
+            Description:  <br></br>
+            <input
+              type="text"
+              name="description"
+              placeholder="Songs that I love"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+            />
+          </label>
+          <p>
+            <button>Create</button>
+          </p>
+        </form>
+      </div>
     </section>
   )
 }
-=======
-        const songDetails = {
-            title: title,
-            description: description,
-            genre: [],
-            songs: []
-        }
-
-        callbackToCreate(songDetails);
-
-        setTitle("");
-        setDescription("");
-
-        navigate(`/playlist/${playlistList.length - 5}`);
-    }
-
-    return (
-        <section className={styles.AddPlaylistPage}>
-            <div className={styles.div}>
-                <h2>Create your own playlist: </h2>
-                <form onSubmit={handleSubmit} className={styles.form}>
-                    <label>
-                        Title: <br></br>
-                        <input
-                            type="text"
-                            name="title"
-                            required
-                            placeholder="Favourites"
-                            value={title}
-                            onChange={e => setTitle(e.target.value)}
-                        />
-                    </label>
-                    <label>
-                        Description:  <br></br>
-                        <input
-                            type="text"
-                            name="description"
-                            placeholder="Songs that I love"
-                            value={description}
-                            onChange={e => setDescription(e.target.value)}
-                        />
-                    </label>
-                    <p>
-                        <button>Create</button>
-                    </p>
-                </form>
-            </div>
-        </section>
-    )
-}
->>>>>>> 898730b1f6004ec0916586699547c6c5e86ca193
