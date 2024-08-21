898730b1f6004ec0916586699547c6c5e86ca193
Jose Luis Neira <jlneira99@gmail.com>
2024-08-21 15:25:27 +0200

    style css for add playlist page

--- src/pages/AddPlaylist/AddPlaylistPage.jsx
+++ src/pages/AddPlaylist/AddPlaylistPage.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./AddPlaylistPage.module.scss"
 
-export default function AddPlaylistPage ({playlistList, callbackToCreate}) {
+export default function AddPlaylistPage({ playlistList, callbackToCreate }) {
 
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
@@ -12,49 +13,51 @@ export default function AddPlaylistPage ({playlistList, callbackToCreate}) {
         e.preventDefault();
 
         const songDetails = {
-          title: title,
-          description: description,
-          genre: [],
-          songs: []
+            title: title,
+            description: description,
+            genre: [],
+            songs: []
         }
 
         callbackToCreate(songDetails);
 
         setTitle("");
         setDescription("");
-    
-        navigate(`/playlist/${playlistList.length-5}`);
+
+        navigate(`/playlist/${playlistList.length - 5}`);
     }
 
     return (
-        <section className="AddMovie box">
-            <h2>Create your own playlist:</h2>
-            <form onSubmit={handleSubmit}>
-            <label>
-                Title:
-                <input 
-                type="text" 
-                name="title" 
-                required
-                placeholder="Favourites" 
-                value={title} 
-                onChange={e => setTitle(e.target.value)}
-                />
-            </label>
-            <label>
-                Description:
-                <input 
-                type="text" 
-                name="description" 
-                placeholder="Songs that I love" 
-                value={description} 
-                onChange={e => setDescription(e.target.value)}
-                />
-            </label>
-            <p>
-                <button>Create</button>
-                </p>
-            </form>
+        <section className={styles.AddPlaylistPage}>
+            <div className={styles.div}>
+                <h2>Create your own playlist: </h2>
+                <form onSubmit={handleSubmit} className={styles.form}>
+                    <label>
+                        Title: <br></br>
+                        <input
+                            type="text"
+                            name="title"
+                            required
+                            placeholder="Favourites"
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Description:  <br></br>
+                        <input
+                            type="text"
+                            name="description"
+                            placeholder="Songs that I love"
+                            value={description}
+                            onChange={e => setDescription(e.target.value)}
+                        />
+                    </label>
+                    <p>
+                        <button>Create</button>
+                    </p>
+                </form>
+            </div>
         </section>
     )
 }
